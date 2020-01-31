92178d97f46bab3a66b2c96536f4a8018d1a409c
venkata anil <anilvenkata@redhat.com>
2020-01-31 14:42:52 +0530

    dpdk osp13 1029p templates

---
+++ dpdk_osp13_1029p/overcloud_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+openstack overcloud deploy \
+--timeout 1200 \
+--templates /usr/share/openstack-tripleo-heat-templates \
+--stack overcloud \
+--libvirt-type kvm \
+--ntp-server clock1.rdu2.redhat.com \
+-r /home/stack/single-nic/roles_data.yaml \
+-e /home/stack/virt/config_lvm.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+-e /home/stack/single-nic/network-environment.yaml \
+-e /home/stack/virt/docker-images.yaml \
+--log-file overcloud_deployment_79.log
